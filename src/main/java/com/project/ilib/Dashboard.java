7b0dd87e164e3a7ea8e1b1a71bb8f65e3651fa85
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-06-13 17:50:19 -0500

    button was added

--- src/main/java/com/project/ilib/Dashboard.java
+++ src/main/java/com/project/ilib/Dashboard.java
@@ -1,15 +1,23 @@
 package com.project.ilib;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Dashboard {
     private JButton button1;
+    private JButton button2;
 
     public Dashboard() {
         button1.addActionListener(new ActionListener() {
             @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        button2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
             }
