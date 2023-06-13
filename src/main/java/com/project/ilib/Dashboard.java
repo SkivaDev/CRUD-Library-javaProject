534ff8f1de89dd2d47771326121a4b1a13d3560d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-06-12 22:03:20 -0500

    ui dependecy

--- src/main/java/com/project/ilib/Dashboard.java
+++ src/main/java/com/project/ilib/Dashboard.java
@@ -1,4 +1,22 @@
 package com.project.ilib;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Dashboard {
+    private JButton button1;
+
+    public Dashboard() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+    }
 }
